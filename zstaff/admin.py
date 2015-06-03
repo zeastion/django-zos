cb16b68bb6462b348c7240f62ad72e192062bad6
zeastion <zeastion@live.cn>
2015-06-03 17:19:42 +0800

    remove zstaff

--- zstaff/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from models import Department, Staff
-
-class StaffAdmin(admin.ModelAdmin):
-    list_display = ('sname', 'gender', 'depart')
-
-admin.site.register(Department)
-admin.site.register(Staff, StaffAdmin)
